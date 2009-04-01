ab50706b6ae7ffdfc399af7d404e0f7245c9735a
sol - Andrii Vasyliev <andrii.vasyliev@gmail.com>
2009-04-01 09:15:59 +0000

    + blacklist, wrong_login, log, log_var, tag2, user_value

--- create.sql
+++ create.sql
@@ -26,11 +26,11 @@ CREATE TABLE prop (
 	name		text		NOT NULL,
 	type_id		integer		NOT NULL,
 	no		integer		DEFAULT 0 NOT NULL,
+	def		text		NULL,
 	is_t		boolean		DEFAULT FALSE NOT NULL,
 	is_n		boolean		DEFAULT FALSE NOT NULL,
 	is_s		boolean		DEFAULT FALSE NOT NULL,
-	is_r		boolean		DEFAULT FALSE NOT NULL,
-	def		text		NULL
+	is_r		boolean		DEFAULT FALSE NOT NULL
 );
 SELECT * INTO del_prop FROM prop LIMIT 0;
 
@@ -43,12 +43,30 @@ CREATE TABLE value (
 );
 SELECT * INTO del_value FROM value LIMIT 0;
 
+CREATE TABLE user_value (
+	id		integer		NOT NULL DEFAULT nextval('id'),
+	obj_id		integer		NOT NULL,
+	user_id		integer		NOT NULL,
+	prop_id		integer		NOT NULL,
+	no		integer		DEFAULT 0 NOT NULL,
+	value		text		DEFAULT '' NOT NULL
+);
+SELECT * INTO del_user_value FROM user_value LIMIT 0;
+
 CREATE TABLE tag (
 	id		integer		NOT NULL DEFAULT nextval('id'),
 	obj_id		integer		NOT NULL,
 	tag_id		integer		NOT NULL
 );
 SELECT * INTO del_tag FROM tag limit 0;
+
+CREATE TABLE tag2 (
+	id		integer		NOT NULL DEFAULT nextval('id'),
+	src_id		integer		NOT NULL,
+	dst_id		integer		NOT NULL,
+	tag_id		integer		NOT NULL
+);
+SELECT * INTO del_tag2 FROM tag2 limit 0;
 
 CREATE TABLE link (
 	obj_id		integer		NOT NULL,
@@ -57,4 +75,34 @@ CREATE TABLE link (
 	tag_id		integer		NOT NULL
 );
 SELECT now()::timestamp without time zone AS old_time,* INTO old_link FROM link limit 0;
+
+CREATE TABLE blacklist (
+	obj_id		integer			NOT NULL,
+	class_id	integer			NOT NULL,
+	user_id		integer			NOT NULL,
+	name		text			NOT NULL,
+	for_myself	boolean			NOT NULL DEFAULT TRUE
+);
+SELECT * INTO del_blacklist FROM blacklist LIMIT 0;
+
+CREATE TABLE wrong_login (
+	login		text			NOT NULL,
+	ip		inet			NOT NULL,
+	time		timestamp		NOT NULL DEFAULT now()
+);
+
+CREATE TABLE log (
+	id		integer			NOT NULL DEFAULT nextval('id'),
+	login		text			NOT NULL,
+	ip		inet			NOT NULL,
+	time		timestamp		NOT NULL,
+	page		text			NOT NULL,
+	was_allowed	boolean			NOT NULL
+);
+
+CREATE TABLE log_var (
+	id		integer			NOT NULL,
+	name		text			NOT NULL,
+	value		text			NOT NULL
+);
 
