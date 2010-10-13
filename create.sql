36b04c33f453cfb772b28b6d9a751e9c55543067
sol - Andrii Vasyliev <andrii.vasyliev@gmail.com>
2010-10-13 16:08:30 +0000

    ! * integer -> bigint + status

--- create.sql
+++ create.sql
@@ -3,115 +3,123 @@
 CREATE SEQUENCE "id" START 1000000;
 
 CREATE TABLE obj (
-	obj_id		integer		DEFAULT nextval('id') NOT NULL,
-	class_id	integer		NOT NULL,
-	label		text		DEFAULT '' NOT NULL,
-	descr		text		DEFAULT '' NOT NULL,
-	create_time	timestamp	DEFAULT now() NOT NULL,
-	update_time	timestamp	DEFAULT now() NOT NULL
+	obj_id		bigint		NOT NULL DEFAULT nextval('id'),
+	class_id	bigint		NOT NULL,
+	label		text		NOT NULL DEFAULT '',
+	descr		text		NOT NULL DEFAULT '',
+	create_time	timestamp	NOT NULL DEFAULT now(),
+	update_time	timestamp	NOT NULL DEFAULT now()
 );
 SELECT now()::timestamp without time zone AS old_time,* INTO old_obj FROM obj limit 0;
 
 CREATE TABLE ref (
-	obj_id		integer		NOT NULL,
-	_id		integer		NOT NULL,
-	name		text		DEFAULT '' NOT NULL,
-	no		integer		DEFAULT 0 NOT NULL
+	obj_id		bigint		NOT NULL,
+	_id		bigint		NOT NULL,
+	name		text		NOT NULL DEFAULT '',
+	no		integer		NOT NULL DEFAULT 0
 );
 SELECT * INTO del_ref FROM ref LIMIT 0;
 
 CREATE TABLE prop (
-	obj_id		integer		NOT NULL,
-	class_id	integer		NOT NULL,
+	obj_id		bigint		NOT NULL,
+	class_id	bigint		NOT NULL,
 	name		text		NOT NULL,
-	type_id		integer		NOT NULL,
-	no		integer		DEFAULT 0 NOT NULL,
+	type_id		bigint		NOT NULL,
+	no		integer		NOT NULL DEFAULT 0,
 	def		text		NULL,
-	is_t		boolean		DEFAULT FALSE NOT NULL,
-	is_n		boolean		DEFAULT FALSE NOT NULL,
-	is_s		boolean		DEFAULT FALSE NOT NULL,
-	is_r		boolean		DEFAULT FALSE NOT NULL
+	is_t		boolean		NOT NULL DEFAULT FALSE,
+	is_n		boolean		NOT NULL DEFAULT FALSE,
+	is_s		boolean		NOT NULL DEFAULT FALSE,
+	is_r		boolean		NOT NULL DEFAULT FALSE
 );
 SELECT * INTO del_prop FROM prop LIMIT 0;
 
 CREATE TABLE value (
-	id		integer		NOT NULL DEFAULT nextval('id'),
-	obj_id		integer		NOT NULL,
-	prop_id		integer		NOT NULL,
-	no		integer		DEFAULT 0 NOT NULL,
-	value		text		DEFAULT '' NOT NULL
+	id		bigint		NOT NULL DEFAULT nextval('id'),
+	obj_id		bigint		NOT NULL,
+	prop_id		bigint		NOT NULL,
+	no		integer		NOT NULL DEFAULT 0,
+	value		text		NOT NULL DEFAULT ''
 );
 SELECT * INTO del_value FROM value LIMIT 0;
 
 CREATE TABLE old_value (
-	obj_id		integer,
-	prop_id		integer,
-	no		integer,
-	value		text,
-	old_time	timestamp,
-	user_id		integer
+	obj_id		bigint		NULL,
+	prop_id		bigint		NULL,
+	no		integer		NULL,
+	value		text		NULL,
+	old_time	timestamp	NULL,
+	subject_id	bigint		NULL
 );
 
 CREATE TABLE user_value (
-	id		integer		NOT NULL DEFAULT nextval('id'),
-	obj_id		integer		NOT NULL,
-	user_id		integer		NOT NULL,
-	prop_id		integer		NOT NULL,
-	no		integer		DEFAULT 0 NOT NULL,
-	value		text		DEFAULT '' NOT NULL
+	id		bigint		NOT NULL DEFAULT nextval('id'),
+	obj_id		bigint		NOT NULL,
+	subject_id	bigint		NOT NULL,
+	prop_id		bigint		NOT NULL,
+	no		integer		NOT NULL DEFAULT 0,
+	value		text		NOT NULL DEFAULT ''
 );
 SELECT * INTO del_user_value FROM user_value LIMIT 0;
 
 CREATE TABLE tag (
-	id		integer		NOT NULL DEFAULT nextval('id'),
-	obj_id		integer		NOT NULL,
-	tag_id		integer		NOT NULL
+	id		bigint		NOT NULL DEFAULT nextval('id'),
+	obj_id		bigint		NOT NULL,
+	tag_id		bigint		NOT NULL
 );
 SELECT * INTO del_tag FROM tag limit 0;
 
 CREATE TABLE tag2 (
-	id		integer		NOT NULL DEFAULT nextval('id'),
-	src_id		integer		NOT NULL,
-	dst_id		integer		NOT NULL,
-	tag_id		integer		NOT NULL
+	id		bigint		NOT NULL DEFAULT nextval('id'),
+	src_id		bigint		NOT NULL,
+	dst_id		bigint		NOT NULL,
+	tag_id		bigint		NOT NULL
 );
 SELECT * INTO del_tag2 FROM tag2 limit 0;
 
 CREATE TABLE link (
-	obj_id		integer		NOT NULL,
-	src_id		integer		NOT NULL,
-	dst_id		integer		NOT NULL,
-	tag_id		integer		NOT NULL
+	obj_id		bigint		NOT NULL,
+	src_id		bigint		NOT NULL,
+	dst_id		bigint		NOT NULL,
+	tag_id		bigint		NOT NULL
+);
+SELECT now()::timestamp without time zone AS old_time,* INTO old_link FROM link limit 0;
+
+CREATE TABLE status (
+	id		bigint		NOT NULL DEFAULT nextval('id'),
+	object_id	bigint		NOT NULL,
+	type_id		bigint		NOT NULL,
+	time		timestamp	NOT NULL DEFAULT now()
 );
 SELECT now()::timestamp without time zone AS old_time,* INTO old_link FROM link limit 0;
 
 CREATE TABLE blacklist (
-	obj_id		integer			NOT NULL,
-	class_id	integer			NOT NULL,
-	user_id		integer			NOT NULL,
-	name		text			NOT NULL,
-	for_myself	boolean			NOT NULL DEFAULT TRUE
+	obj_id		bigint		NOT NULL,
+	class_id	bigint		NOT NULL,
+	subject_id	bigint		NOT NULL,
+	name		text		NOT NULL,
+	for_myself	boolean		NOT NULL DEFAULT TRUE
 );
 SELECT * INTO del_blacklist FROM blacklist LIMIT 0;
 
 CREATE TABLE wrong_login (
-	login		text			NOT NULL,
-	ip		inet			NOT NULL,
-	time		timestamp		NOT NULL DEFAULT now()
+	login		text		NOT NULL,
+	ip		inet		NOT NULL,
+	time		timestamp	NOT NULL DEFAULT now()
 );
 
 CREATE TABLE log (
-	id		integer			NOT NULL DEFAULT nextval('id'),
-	login		text			NOT NULL,
-	ip		inet			NOT NULL,
-	time		timestamp		NOT NULL,
-	page		text			NOT NULL,
-	was_allowed	boolean			NOT NULL
+	id		bigint		NOT NULL DEFAULT nextval('id'),
+	login		text		NOT NULL,
+	ip		inet		NOT NULL,
+	time		timestamp	NOT NULL,
+	page		text		NOT NULL,
+	was_allowed	boolean		NOT NULL
 );
 
 CREATE TABLE log_var (
-	id		integer			NOT NULL,
-	name		text			NOT NULL,
-	value		text			NOT NULL
+	id		bigint		NOT NULL,
+	name		text		NOT NULL,
+	value		text		NOT NULL
 );
 
