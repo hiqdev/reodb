39314298b5d0dd4ec3a6735d42c2765a555fac8d
sol - Andrii Vasyliev <andrii.vasyliev@gmail.com>
2010-10-13 13:34:23 +0000

    + o.label

--- views.sql
+++ views.sql
@@ -46,8 +46,9 @@ CREATE OR REPLACE VIEW value_h AS
 
 -- BLACKLIST
 CREATE OR REPLACE VIEW blacklist_h AS
-	SELECT		b.obj_id,t.name AS class,object_full_name(b.user_id) AS user,b.name,to_sign(b.for_myself) AS for_myself
+	SELECT		b.obj_id,t.name AS class,object_full_name(b.user_id) AS user,b.name,to_sign(b.for_myself) AS for_myself,o.label
 	FROM		blacklist	b
+	JOIN		obj		o ON o.obj_id=b.obj_id
 	LEFT JOIN	ref		t ON t.obj_id=b.class_id
 ;
 
