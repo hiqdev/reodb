b4b9659eeb11458ab7e5ea8da6851a4c73e4d9d9
sol - Andrii Vasyliev <andrii.vasyliev@gmail.com>
2014-01-18 13:26:53 +0000

    improved prop_h view

--- views.sql
+++ views.sql
@@ -27,11 +27,11 @@ CREATE OR REPLACE VIEW state_h AS
 
 -- PROP
 CREATE OR REPLACE VIEW prop_h AS
-	SELECT		obj_id,no,class_full_name(p.class_id) AS class,name,ref_full_name(type_id) AS type,
-			to_sign(is_in_table)||to_sign(can_be_null)||to_sign(is_required)||to_sign(is_repeated) AS TNSR,
-			label,''''||def||'''' AS def
+	SELECT		p.obj_id,p.no,p.class_id,class_full_name(p.class_id) AS class,name,ref_full_name(p.type_id) AS type,
+                to_sign(p.is_in_table)||to_sign(p.can_be_null)||to_sign(p.is_required)||to_sign(p.is_repeated) AS TNSR,
+                o.label,''''||p.def||'''' AS def
 	FROM		prop	p
-	LEFT JOIN	obj	o USING (obj_id)
+	LEFT JOIN	obj     o ON o.obj_id=p.obj_id
 	ORDER BY	class,no
 ;
 
