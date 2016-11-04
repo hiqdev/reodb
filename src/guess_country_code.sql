a6128ba4a3364bd642be17e0f54ca36429b61d03
Andrii Vasyliev <sol@hiqdev.com>
2016-11-04 16:44:14 +0000

    add guess_country_code_name_pkey

--- src/guess_country_code.sql
+++ src/guess_country_code.sql
@@ -2,8 +2,8 @@ CREATE TABLE guess_country_code (
     cc      text not null,
     name    text not null
 );
-CREATE INDEX  guess_country_code_cc_index ON guess_country_code (cc);
-CREATE INDEX  guess_country_code_name_index ON guess_country_code (name);
+ALTER TABLE guess_country_code ADD CONSTRAINT guess_country_code_name_pkey PRIMARY KEY (name);
+CREATE INDEX guess_country_code_cc_index ON guess_country_code (cc);
 
 CREATE OR REPLACE FUNCTION guess_country_code(a_name text) RETURNS text AS $$
     SELECT cc FROM guess_country_code WHERE name=lower($1 COLLATE "ru_RU.UTF-8");
