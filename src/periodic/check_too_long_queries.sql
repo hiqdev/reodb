ac97406acfb371d5c5da092aeb7440fc44cc5a7b
Andrii Vasyliev <sol@hiqdev.com>
2017-04-11 09:59:48 +0000

    improved query for check_too_long_queries

--- src/periodic/check_too_long_queries.sql
+++ src/periodic/check_too_long_queries.sql
@@ -1,5 +1,9 @@
-SELECT      'REDALERT too long queries', query_start, query
+SELECT      'REDALERT too long queries',
+            date_trunc('second', query_start::timestamp) AS started,
+            date_trunc('second', now()-query_start) AS duration,
+            query
 FROM        pg_stat_activity
 WHERE       state!='idle'
     AND     query_start+'1minute'<now()
+ORDER BY	duration DESC
 ;
