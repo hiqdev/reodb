ac371b115b4d1f3944c116bee5049d6c5fc8c506
Andrii Vasyliev <sol@hiqdev.com>
2017-09-12 06:54:45 +0000

    minor

--- src/periodic/check_too_long_queries.sql
+++ src/periodic/check_too_long_queries.sql
@@ -1,10 +1,10 @@
 SELECT      'REDALERT too long queries',
-            date_trunc('second', query_start::timestamp) AS started,
             date_trunc('second', now() - query_start) AS duration,
+            date_trunc('second', query_start::timestamp) AS started,
             query
 FROM        pg_stat_activity
 WHERE       state != 'idle'
-    AND     query_start + '2minute' < now()
-    AND     query NOT IN (SELECT query FROM allowed_long_query)
-ORDER BY	duration DESC
+        AND query_start + '2minute' < now()
+        AND query NOT IN (SELECT query FROM allowed_long_query)
+ORDER BY    duration DESC
 ;
