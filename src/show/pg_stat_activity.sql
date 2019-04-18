01505fbe97a94b5f648f7df46400ba8789c80d1e
Andrii Vasyliev <sol@hiqdev.com>
2019-04-18 09:46:00 +0000

    Improved `show/pg_stat_activity`: removed current process from list

--- src/show/pg_stat_activity.sql
+++ src/show/pg_stat_activity.sql
@@ -1,5 +1,7 @@
 SELECT		date_trunc('second',now()-query_start) AS duration,pid,query
 FROM		pg_stat_activity
-WHERE		query!='<IDLE>' AND state!='idle'
+WHERE		query != '<IDLE>'
+        AND state != 'idle'
+        AND pid != pg_backend_pid()
 ORDER BY	duration DESC
 ;
