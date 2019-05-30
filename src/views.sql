ce60b1fba502f476c768b02d24beb3f94ac216db
Andrii Vasyliev <sol@hiqdev.com>
2019-05-30 14:41:32 +0200

    Improved `tie_h` view

--- src/views.sql
+++ src/views.sql
@@ -95,7 +95,9 @@ CREATE OR REPLACE VIEW errors AS
 --- TIE
 CREATE OR REPLACE VIEW tie_h AS
     SELECT      x.id,x.src_id,x.dst_id,x.tag_id,
-                obj_full_name(x.src_id) AS src,t.name AS tag,obj_full_name(x.dst_id) AS dst
+                obj_full_name(x.src_id) AS src,
+                coalesce(t.name, obj_full_name(x.tag_id)) AS tag,
+                obj_full_name(x.dst_id) AS dst
     FROM        tie         x
     LEFT JOIN   ref         t ON t.obj_id=x.tag_id
 ;
