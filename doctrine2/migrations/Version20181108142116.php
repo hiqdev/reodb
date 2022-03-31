38bd508ecae842d5493ed5075c14f59eec55b4d9
SilverFire - Dmitry Naumenko <d.naumenko.a@gmail.com>
2022-03-31 19:01:35 +0300

    Upgrade to actual Doctrine API

--- doctrine2/migrations/Version20181108142116.php
+++ doctrine2/migrations/Version20181108142116.php
@@ -7,19 +7,19 @@ use Doctrine\Migrations\AbstractMigration;
 
 class Version20181108142116 extends AbstractMigration
 {
-    public $extensions = [
+    public array $extensions = [
         'pgcrypto',
         'hstore',
     ];
 
-    public function up(Schema $schema)
+    public function up(Schema $schema): void
     {
         foreach ($this->extensions as $name) {
             $this->addSql('CREATE EXTENSION IF NOT EXISTS ' . $name);
         }
     }
 
-    public function down(Schema $schema)
+    public function down(Schema $schema): void
     {
         foreach ($this->extensions as $name) {
             $this->addSql('DROP EXTENSION IF EXISTS ' . $name);
