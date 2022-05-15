afa110088ba313b0fca35cf4c941763e9de40dfa
Andrii Vasyliev <sol@hiqdev.com>
2022-05-15 15:33:13 +0000

    fix show/reltables to work with namespaced tables

--- src/show/reltables.sql
+++ src/show/reltables.sql
@@ -1,5 +1,5 @@
-SELECT      relname, relpages, reltuples,
-            pg_size_pretty(pg_table_size(relname::regclass)) AS size
+SELECT      relname, relpages, reltuples, relfilenode,
+            pg_size_pretty(pg_table_size(ns.nspname||'.'||relname)) AS size
 FROM        pg_class        cl
 LEFT JOIN   pg_namespace    ns ON ns.oid = cl.relnamespace
 WHERE       nspname NOT IN ('information_schema')
